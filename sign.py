4819235ea278ea0734518af11c13a65ac1684d5d
Sameer Puri <sameer@users.noreply.github.com>
2019-04-10 14:10:28 -0500

    Fix incorrect dict keys

--- sign.py
+++ sign.py
@@ -13,11 +13,11 @@ def is_open(last_poller_json, mentors_on_duty: List[Shift]) -> bool:
         return False
 
     # Normal opne (follow shift schedule)
-    if last_poller_json['sign']['one_on'] == 1:
+    if last_poller_json['sign']['switch']['one_on'] == 1:
         return len(mentors_on_duty) > 0
 
     # Force open
-    if last_poller_json['sign']['two_on'] == 1:
+    if last_poller_json['sign']['switch']['two_on'] == 1:
         return True
 
     # Force closed
