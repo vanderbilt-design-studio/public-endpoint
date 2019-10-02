7298f1b8a12ae5aabda48888a73f55f645628a8f
Sameer Puri <sameer@users.noreply.github.com>
2019-10-02 13:33:30 -0500

    Add FORCE_CLOSED enum state (could be useful for 'opens at')

--- sign.py
+++ sign.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 from enum import Enum, auto
 
 from mentors import Shift, get_mentors_on_duty
@@ -8,24 +8,26 @@ class OpenType(Enum):
     CLOSED = auto()
     OPEN = auto()
     FORCE_OPEN = auto()
+    FORCE_CLOSE = auto()
 
 
-def is_open(last_poller_json, mentors_on_duty: List[Shift]) -> OpenType:
+def is_open(poller_json: Dict, mentors_on_duty: List[Shift]) -> OpenType:
+    """Runs decision logic for whether the sign should say open"""
     # JSON doesn't tell us anything about the sign, should never happen though
-    if 'sign' not in last_poller_json:
+    if 'sign' not in poller_json:
         return OpenType.OPEN if len(mentors_on_duty) > 0 else OpenType.CLOSED
 
     # Never open when the door is not open
-    if last_poller_json['sign']['door'] == 1:
+    if poller_json['sign']['door'] == 1:
         return OpenType.CLOSED
 
     # Normal open (follow shift schedule)
-    if last_poller_json['sign']['switch']['one_on'] == 1:
+    if poller_json['sign']['switch']['one_on'] == 1:
         return OpenType.OPEN if len(mentors_on_duty) > 0 else OpenType.CLOSED
 
     # Force open
-    if last_poller_json['sign']['switch']['two_on'] == 1:
+    if poller_json['sign']['switch']['two_on'] == 1:
         return OpenType.FORCE_OPEN
 
     # Force closed
-    return OpenType.CLOSED
+    return OpenType.FORCE_CLOSE
