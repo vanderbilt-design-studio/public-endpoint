c4258775ae27d10125ffaa24150cfb68a6f1b7b3
Sameer Puri <sameer@users.noreply.github.com>
2019-10-29 12:57:50 -0500

    Clarify sheets code, retain old values if no report for card code found

--- sheets.py
+++ sheets.py
@@ -1,7 +1,8 @@
 import os
 import re
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import json
+import logging
 
 from anchorlink import Attendance, ReportLine, Credentials, Event
 from googleapiclient import discovery
@@ -17,7 +18,7 @@ VALUE_INPUT_OPTION = 'RAW'
 class Sheet():
     def __init__(self, credentials: service_account.Credentials, spreadsheet_url: str, swipe_card_codes_column: str, prefix: str = ''):
         '''
-        Uses Google Spreadsheets API v4 to update new card number entries by adding
+        Uses Google Spreadsheets API v4 to update new card number entries by populating fields
         credentials: Google Service Account credentials
         spreadsheet_url: URL of the form https://docs.google.com/spreadsheets/d/spreadsheetId/edit#gid=sheetId
         swipe_card_codes_column: uppercase alphabetic column name
@@ -47,43 +48,82 @@ class Sheet():
     #         'resourceUri': f'https://www.googleapis.com/drive/v3/files/{resource_id}',
     #         'expiration' : int(round((datetime.datetime.now() + duration).time() * 1000))
     #     })
-    
+    def get_column_names(self) -> List[str]:
+        '''Gets first row of spreadsheet, which is assumed to be column names. This is standard behavior of spreadsheets created through Google Forms.'''
+        google_form_column_names: List[List[str]] = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range='1:1', majorDimension='ROWS').execute().get('values', [])
+        return google_form_column_names[0]
+
+    def get_column_values(self, column_id: str) -> List[str]:
+        '''Get cell values from a column.'''
+        values = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=f'{column_id}:{column_id}', majorDimension='COLUMNS').execute().get('values', [])
+        values = values[0][1:]
+        return values
+
+    def get_swipe_card_codes(self) -> List[str]:
+        '''Gets cell values from the swipe card codes column. Strips whitespace and skips the first value (column name)'''
+        swipe_card_codes = self.get_column_values(self.swipe_card_codes_column)
+        return list(map(lambda code: code.strip(), swipe_card_codes))
+
+    def is_column_valid(self, column_name: str) -> bool:
+        '''Checks if a column name, barring the prefix, is one of the ReportLine fields'''
+        if not column_name.startswith(self.prefix):
+            return False
+        if column_name.replace(self.prefix, '', 1) not in ReportLine._fields:
+            return False
+        return True
+
     def update(self, attendance: Attendance):
-        swipe_card_codes = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=f'{self.swipe_card_codes_column}:{self.swipe_card_codes_column}', majorDimension='COLUMNS').execute().get('values', [])
-        swipe_card_codes = swipe_card_codes[0][1:]
+        swipe_card_codes = self.get_swipe_card_codes()
         if len(swipe_card_codes) == 0:
             return
+        logging.info('Getting columns')
+        column_names = self.get_column_names()
 
-        google_form_column_names: List[str] = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range='1:1', majorDimension='ROWS').execute().get('values', [])
-        google_form_column_names = google_form_column_names[0]
-        
-        google_form_filtered_column_names = []
-        for i, column_name in enumerate(google_form_column_names):
-            if not column_name.startswith(self.prefix):
+        filtered_column_names: List[Tuple[str, str]] = []
+
+        for i, column_name in enumerate(column_names):
+            if not self.is_column_valid(column_name):
                 continue
-            if column_name.replace(self.prefix, '', 1) not in ReportLine._fields:
-                continue
-            google_form_filtered_column_names.append((column_number_to_id(i + 1), column_name))
-        if len(google_form_filtered_column_names) == 0:
+            filtered_column_names.append((column_number_to_id(i + 1), column_name))
+        if len(filtered_column_names) == 0:
             return
 
+        logging.info('Uploading uniquified codes')
         attendance.upload(list(set(filter(lambda code: code.isdigit(), swipe_card_codes))))
+
+        logging.info('Downloading report and getting a report line for each sheet row')
         report: List[ReportLine] = attendance.download()
         code_to_report: Dict[str, ReportLine] = {line.Card_ID_Number: line for line in report}
-        report_rows = [code_to_report[code] if code in code_to_report else None for code in swipe_card_codes]
+        report_rows: List[ReportLine] = [code_to_report.get(code, None) for code in swipe_card_codes]
+
+        logging.info('Getting original column values')
+        column_name_to_original_values: Dict[str, List[str]] = {}
+        for column_id, column_name in filtered_column_names:
+            values = self.get_column_values(column_id)
+            if len(values) < len(report_rows): # Fill with empty values if not as long as report_rows
+                values += [''] * (len(report_rows) - len(values))
+            column_name_to_original_values[column_name] = values
+
         data = []
-        for column_id, column_name in google_form_filtered_column_names:
+        for column_id, column_name in filtered_column_names:
+            values = column_name_to_original_values[column_name]
+            for i, line in enumerate(report_rows):
+                # Update cell values IFF a report exists
+                if line is not None:
+                    values[i] = getattr(line, column_name.replace(self.prefix, '', 1))
             data.append({
                 "range": f'{column_id}2:{column_id}{len(report_rows)+1}',
                 "majorDimension": "COLUMNS",
-                "values": [list(map(lambda line: getattr(line, column_name.replace(self.prefix, '', 1)) if line is not None else '', report_rows))]
+                "values": values
             })
         batch_body = {
             'value_input_option': VALUE_INPUT_OPTION,
             'data': data
         }
+
         batch_json = json.dumps(batch_body)
-        if self.last_batch_json is None or self.last_batch_json != batch_json:
+        if False and (self.last_batch_json is None or self.last_batch_json != batch_json):
+            logging.info('Posting updated cell values')
             self.service.spreadsheets().values().batchUpdate(spreadsheetId=self.spreadsheet_id, body=batch_body).execute()
             self.last_batch_json = batch_json
 
@@ -103,6 +143,7 @@ def column_number_to_id(n: int) -> str:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     attendance = Attendance(Credentials('puris', os.environ['PASS']), Event('designstudio', '5048888'))
     printee_sheet = Sheet(service_account.Credentials.from_service_account_file('credentials.json'), 'https://docs.google.com/spreadsheets/d/1tHCdRLZk4owYvN20t5A41__YDU-kBjw2Rw2E9y9SXhI/edit#gid=886233104', 'B', 'Printee_')
     printee_sheet.update(attendance)
